fe2c79f895bac39a95cf946ea6792985c58aabae
Mohamed Elfar <melfar391@gmail.com>
2021-09-11 20:45:19 +0200

    Add project files.

---
+++ Core/Entities/Address.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Entities
+{
+    public class Address :EntityBase
+    {
+        public string City { get; set; }
+        public string Street { get; set; }
+        public int Number { get; set; }
+    }
+}
